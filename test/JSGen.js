6bead295e79f85f470d24e2c31e11b28260ca1b3
niole <niolenelson@gmail.com>
2017-07-04 21:45:50 -0700

    creates tests for number, refactors test implementation

--- test/JSGen.js
+++ test/JSGen.js
@@ -7,30 +7,27 @@ const {
   Array,
 } = require('../JSGen.js');
 
+function assertMany(N, assertion) {
+  for (let i = 0; i < N; i++) {
+    assertion();
+  }
+}
+
 describe('Boolean generator', function() {
   it('should be able to create a random Boolean generator', function() {
     const b = Boolean();
 
-    assert.isBoolean(b.Next(), "should be a boolean");
-    assert.isBoolean(b.Next(), "should be a boolean");
-    assert.isBoolean(b.Next(), "should be a boolean");
+    assertMany(100, () => assert.isBoolean(b.Next(), "should be a boolean"));
   });
 
   it('should be able to create a true Boolean generator', function() {
     const b = Boolean(true);
-
-    assert.strictEqual(b.Next(), true, "should be true");
-    assert.strictEqual(b.Next(), true, "should be true");
-    assert.strictEqual(b.Next(), true, "should be true");
+    assertMany(100, () => assert.strictEqual(b.Next(), true, "should be true"));
   });
 
   it('should be able to create a false Boolean generator', function() {
-
     const b = Boolean(false);
-
-    assert.strictEqual(b.Next(), false, "should be false");
-    assert.strictEqual(b.Next(), false, "should be false");
-    assert.strictEqual(b.Next(), false, "should be false");
+    assertMany(100, () => assert.strictEqual(b.Next(), false, "should be false"));
   });
 
 });
@@ -38,30 +35,19 @@ describe('Boolean generator', function() {
 describe('String generator', function() {
   it('should be able to create a random String generator', function() {
     const s = String();
-
-    assert.isString(s.Next(), "must be a string");
-    assert.isString(s.Next(), "must be a string");
-    assert.isString(s.Next(), "must be a string");
-
+    assertMany(100, () => assert.isString(s.Next(), "must be a string"));
     assert.notStrictEqual(s.Next(), s.Next(), "strings must not match");
   });
 
   it('should be able to return specified string', function() {
     const string = 'cat';
     const s = String(string);
-
-    assert.strictEqual(s.Next(), string, `string should be ${string}`);
-    assert.strictEqual(s.Next(), string, `string should be ${string}`);
-    assert.strictEqual(s.Next(), string, `string should be ${string}`);
+    assertMany(100, () => assert.strictEqual(s.Next(), string, `string should be ${string}`));
   });
 
   it('should return string of specific length', function() {
     const s = String().Length(5);
-
-    assert.strictEqual(s.Next().length, 5, "length should be 5");
-    assert.strictEqual(s.Next().length, 5, "length should be 5");
-    assert.strictEqual(s.Next().length, 5, "length should be 5");
-
+    assertMany(100, () => assert.strictEqual(s.Next().length, 5, "length should be 5"));
     assert.notStrictEqual(s.Next(), s.Next(), "strings must not match");
   });
 
@@ -81,3 +67,52 @@ describe('String generator', function() {
   });
 });
 
+describe('Number generator', function() {
+  it('should be able to generate Numbers', function() {
+    const n = Number();
+    assertMany(100, () => assert.isNumber(n.Next(), "should be a number"));
+    assert.notStrictEqual(n.Next(), n.Next(), "shouldn't be equal");
+  });
+
+  it('should be able to generate constat Numbers', function() {
+    const n = Number(89);
+    assertMany(100, () => assert.strictEqual(n.Next(), 89, "should be 89"));
+  });
+
+  it('should be able to generate only negatives', function() {
+    const n = Number().Negative();
+    assertMany(100, () => assert.isTrue(n.Next() < 0, "should be negative"));
+    assert.notStrictEqual(n.Next(), n.Next(), "shouldn't be equal");
+  });
+
+  it('should be able to only generate positives', function() {
+    const n = Number().Positive();
+    assertMany(100, () => assert.isTrue(n.Next() > -1, "should be positive"));
+    assert.notStrictEqual(n.Next(), n.Next(), "shouldn't be equal");
+  });
+
+  it('should be able to generate enum of Numbers', function(done) {
+    const enm = [1,55,864,-2455,0];
+    const n = Number().Enum.apply(null, enm);
+
+    while(enm.length > 0) {
+      const found = enm.indexOf(n.Next());
+      if (found > -1) {
+        enm.splice(found, 1);
+      }
+    }
+    assert.notOk(enm.length, "returned all elements in enum");
+    done();
+  });
+
+  it('should be able to generate within a range of Numbers', function() {
+    const below = 6;
+    const atLeast = -5;
+    const n = Number().Range(atLeast, below);
+    assertMany(100, () => {
+      const output = n.Next();
+      assert.isBelow(output, below, `should be below ${below}`);
+      assert.isAtLeast(output, atLeast, `should be at least ${atLeast}`);
+    });
+  });
+});
